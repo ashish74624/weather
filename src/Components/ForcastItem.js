4272a01bac542c1d1232a6321a77fc77b58a97fb
Ashish <ashish74624@gmail.com>
2023-08-30 02:17:08 +0530

    Added new bg routes to the forcast section

--- src/Components/ForcastItem.js
+++ src/Components/ForcastItem.js
@@ -7,7 +7,7 @@ function ForcastItem(props) {
                 {props.text}
                 {props.date}
             </div> */}
-            <img className='absolute -z-20 h-36 w-36 lg:h-44 lg:w-44 overflow-hidden rounded-lg' src={`https://image-get.vercel.app/Day/${props.imgText}.webp`} alt="Background" />
+            <img className='absolute -z-20 h-36 w-36 lg:h-44 lg:w-44 overflow-hidden rounded-lg' src={`https://image-get.onrender.com/ashishkumar74624@gmail.com/Day/${props.imgText}.webp`} alt="Background" />
             <div className="h-36 w-36 lg:h-44 lg:w-44 bg-cover  group overflow-hidden rounded-lg"
                 id="HOVER">
                 <div className="  h-full w-full bg-black/30 text-white flex-col flex items-center justify-center bottom-0  " id="hover" >
